0c3207ae9185d8ee65861e4a19eefb5a2ed0dc68
Nisal <thenisalb@gmail.com>
2022-09-17 22:41:10 +0530

    exercise(1.4): fix issues in bad.c

--- src/chapter1/bad.c
+++ src/chapter1/bad.c
@@ -1,12 +1,17 @@
+/* Fix 1: Add headers */
+#include <stdio.h>
 
-void main(void)
+/* Fix 2: Set return type for main */
+int main(void)
 {
 	int i;
 	double A[5] = {
 		9.0,
 		2.9,
-		3.E+25,
-		.0007
+		/* Fix 3: Order of elements (without designated elements) */
+		0,
+		.0007,
+		3.E+25
 	};
 
 	for (i = 0; i < 5; ++i) {
